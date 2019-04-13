94f066a42a3ad596024cdbd6b82e2db9ffed7da5
Vasyl S <vspachyn@gmail.com>
2019-04-14 00:03:57 +0300

    Added Enemy animation and movement

---
+++ BetterTomorrow/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : PhysicsObject
+{
+    public float maxSpeed = 1;
+    public float walkingRange = 200;
+
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private bool facingLeft = true;
+    private float flipCounter = 0;
+
+    // Use this for initialization
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        flipCounter = flipCounter + 1;
+        if (flipCounter > walkingRange)
+        {
+            flipCounter = 0;
+            facingLeft = !facingLeft;
+            Flip();
+        }
+
+        move.x = facingLeft ? -1 : 1;
+        targetVelocity = move * maxSpeed;
+    }
+
+    private void Flip()
+    {
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+    }
+}
