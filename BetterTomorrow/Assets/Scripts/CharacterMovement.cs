ccc0d93e82764d9afe383373fed21c540c5c483e
taras42 <taras.my.mail@gmail.com>
2019-04-15 02:12:48 +0300

    Added pixel perfect camera. Refactor character movement.

---
+++ BetterTomorrow/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public CharacterController2D controller;
+    public Animator animator;
+
+    float horizontalMove = 0f;
+
+    public float runSpeed = 15f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(horizontalMove);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
---
+++ BetterTomorrow/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float maxSpeed = 10;
+    public float walkingRange = 200;
+    public CharacterController2D controller;
+    public Animator animator;
+
+    private float flipCounter = 0f;
+    float horizontalMove = -1f;
+    float direction = 0f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        flipCounter = flipCounter + 1;
+
+        if (flipCounter > walkingRange)
+        {
+            flipCounter = 0;
+            horizontalMove = horizontalMove * -1f;
+        }
+
+        direction = horizontalMove * maxSpeed;
+
+        //animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(direction * Time.fixedDeltaTime, false, false);
+    }
+}
